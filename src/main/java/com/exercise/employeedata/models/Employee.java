ae42de79fafbb9df070cb23f689225d8fe00b7cb
RRoager <rasmus-roager@hotmail.com>
2023-03-09 15:28:34 +0100

    Added validation on fields

---
+++ src/main/java/com/exercise/employeedata/models/Employee.java
@@ -0,0 +1,30 @@
+package com.exercise.employeedata.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String address;
+    @NotNull
+    private String department;
+}
