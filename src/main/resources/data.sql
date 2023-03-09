c14524e05312bd93974735c29d3bd3576a3890de
RRoager <rasmus-roager@hotmail.com>
2023-03-09 15:27:58 +0100

    Added db, data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into employees (name, address, department) values ('Rasmus Roager', 'Copenhagen, Denmark', 'IT');
+insert into employees (name, address, department) values ('Bjarne Goldbaek', 'Barcelona, Spain', 'IT');
+insert into employees (name, address, department) values ('Torkil Tyrring', 'Paris, France', 'HR');
+insert into employees (name, address, department) values ('Lone Hansen', 'Aarhus, Denmark', 'Management');
